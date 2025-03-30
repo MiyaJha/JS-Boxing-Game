6be67830af7c77cdf56523f683fd9636b87ddb81
Jhamiya Jones <jhamiyaj@gmail.com>
2025-03-30 14:32:34 +0000

    added README & js step one

---
+++ boxing.js
@@ -0,0 +1,18 @@
+let player1 = {
+    name: "Player 1",
+    health: 100,
+    };
+let player2 = {
+    name: "Player 2",
+    health: 100,
+} ;  
+let currentPlayer = player1;
+ 
+
+function generateDamage() {
+    return Math.floor(Math.random() * 10) + 1; 
+    }
+
+
+
+    
